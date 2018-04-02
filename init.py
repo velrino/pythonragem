22fd83b5cf2b0eaa4e0177c142c695cd134ae107
velrino <denismagalhaesvelrino@gmail.com>
2018-04-02 01:06:31 -0300

    Indenting and improving the code

--- init.py
+++ init.py
@@ -6,29 +6,31 @@ import HelloWolrd
 
 
 def run():
-	menu()
-	option = input('Choose your option: ')
-	choose(option)
+    menu()
+    option = input('Choose your option: ')
+    choose(option)
+    return;
 
 def choose(value):
-	if value == 1:
-		HelloWolrd.main()
-	elif value == 2:
-	   print "2 - Not implemented"
-	elif value == 3:
-	   print "3 - Not implemented"
-	else:
-	   print "Invalid option"
-	   run()
-
+    if value == 1:
+        HelloWolrd.main()
+    elif value == 2:
+        print '2 - Not implemented'
+    elif value == 3:
+        print '3 - Not implemented'
+    else:
+        print 'Invalid option'
+        run()
+	return;
 
 def menu():
-	print("************************")
-	print "Your Options"
-	print("************************")
-	print "1 - Hello Wolrd"
-	print "2 - Not implemented"
-	print "3 - Not implemented"
-	print("************************")
+    print '************************'
+    print 'Your Options'
+    print '************************'
+    print '1 - Hello Wolrd'
+    print '2 - Not implemented'
+    print '3 - Not implemented'
+    print '************************'
+    return;
 
 run()
