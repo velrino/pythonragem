3a1b25f2fc5ca131ca192715b40e8325585a94db
velrino <denismagalhaesvelrino@gmail.com>
2018-04-02 00:58:59 -0300

    Adding menu to choose what to perform

--- init.py
+++ init.py
@@ -4,4 +4,31 @@ sys.path.insert(0, './lib')
 
 import HelloWolrd
 
-HelloWolrd.main()
+
+def run():
+	menu()
+	option = input('Choose your option: ')
+	choose(option)
+
+def choose(value):
+	if value == 1:
+		HelloWolrd.main()
+	elif value == 2:
+	   print "2 - Not implemented"
+	elif value == 3:
+	   print "3 - Not implemented"
+	else:
+	   print "Invalid option"
+	   run()
+
+
+def menu():
+	print("************************")
+	print "Your Options"
+	print("************************")
+	print "1 - Hello Wolrd"
+	print "2 - Not implemented"
+	print "3 - Not implemented"
+	print("************************")
+
+run()
